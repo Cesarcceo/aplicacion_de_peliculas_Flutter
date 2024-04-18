5e2aa46c33ae4186abc92464461d0a031fd3f415
Cesario54 <cesar.consuelo@cceo.com.mx>
2024-04-18 17:56:12 -0600

    New feature (lib): :sparkles: se creo la carpeta models junto con los archivos now_playing_response.dart, movie.dart y el archivo de exportaciones de la carpeta. se conecto exitosamente la base de datos de la web de peliculas con la aplicacion

---
+++ lib/models/models.dart
@@ -0,0 +1,3 @@
+
+export 'package:aplicacion_de_peliculas/models/movie.dart';
+export 'package:aplicacion_de_peliculas/models/now_playing_response.dart';
--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,14 +1,31 @@
+import 'dart:convert';
+import 'package:aplicacion_de_peliculas/models/models.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier{
+  
+  String _apiKey='6c39d0721c26c0eda41d05d6caef56a8';
+  String _baseUrl='api.themoviedb.org';
+  String _language='es-ES';
 
   MoviesProvider(){
-    
+
     print('MoviesProvider inicializando');
     this.getOnDisplayMovies();
   }
 
   getOnDisplayMovies() async {
-    print('getOnDisplayMovies');
+    var url = Uri.https(_baseUrl, '3/movie/now_playing', {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': '1',
+    });
+
+  // Await the http get response, then decode the json-formatted response.
+  final response = await http.get(url);
+  final nowPlayinResponse = NowPlayingResponse.fromJson(response.body);
+
+  print(nowPlayinResponse.results[1].title);
   }
 }
