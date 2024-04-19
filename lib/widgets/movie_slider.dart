021fbf19d36b0268181dcb9137440f396a6961af
Cesario54 <cesar.consuelo@cceo.com.mx>
2024-04-19 17:23:25 -0600

    New feature (movie_slider.dart): :sparkles: las peliculas populares ya se muestran en pantalla

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -1,6 +1,19 @@
+import 'package:aplicacion_de_peliculas/models/models.dart';
+import 'package:aplicacion_de_peliculas/models/movie.dart';
 import 'package:flutter/material.dart';
 
 class MovieSlider extends StatelessWidget {
+
+  final List<Movie> movies;
+  final String? title;
+
+  const MovieSlider({
+    super.key, 
+    required this.movies, 
+    this.title
+    });
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -9,19 +22,20 @@ class MovieSlider extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Text('populares',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-          ),
+          if(title!=null)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Text(title!,
+                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            ),
           const SizedBox(
             height: 5,
           ),
           Expanded(
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              itemCount: 20,
-              itemBuilder: (_, int index) => _MoviePoster(),
+              itemCount: movies.length,
+              itemBuilder: (_, int index) => _MoviePoster(movie: movies[index]),
             ),
           ),
         ],
@@ -31,6 +45,14 @@ class MovieSlider extends StatelessWidget {
 }
 
 class _MoviePoster extends StatelessWidget {
+
+  final Movie movie;
+
+  const _MoviePoster({
+    super.key, 
+    required this.movie
+    });
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -44,9 +66,9 @@ class _MoviePoster extends StatelessWidget {
                 arguments: 'movie-instance'),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20),
-              child: const FadeInImage(
-                placeholder: AssetImage('assets/no-image.jpg'),
-                image: NetworkImage('https://via.placeholder.com/300x400'),
+              child: FadeInImage(
+                placeholder: const AssetImage('assets/no-image.jpg'),
+                image: NetworkImage(movie.fullPosterImg),
                 width: 130,
                 height: 180,
                 fit: BoxFit.cover,
@@ -54,8 +76,8 @@ class _MoviePoster extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 5),
-          const Text(
-            'Mama coco y la invacion de las abuelas',
+          Text(
+            movie.title,
             maxLines: 2,
             overflow: TextOverflow.ellipsis,
             textAlign: TextAlign.center,
